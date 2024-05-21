994e5ad2022d573211e298975f25ff369f1e0477
Andrii Melashchenko <6060861+javatask@users.noreply.github.com>
2024-05-21 13:57:47 +0200

    Update big_picture.py
    Improve Big Picture diagram

--- diagrams/big_picture.py
+++ diagrams/big_picture.py
@@ -7,18 +7,39 @@ from diagrams.generic.network import *
 from diagrams.aws.iot import *
 from diagrams.aws.general import *
 
+graph_attr = {
+    "dpi": "200",
+    "size": "500, 300",
+    "fontsize": "40",
+    "labelfontsize":"30",
+    "margin":"0",
+    "pad": "0"
+}
 
-with Diagram("AWS SiteWise", show=False) as diag:
-    with Cluster("On-prem") as on_prem:
-        with Cluster("OT Network"):
-            ot_net = Switch("HiOS 1") >> Subnet("OT Network 1") >> Firewall("Firewall 1")
+cluster_graph_attr = {
+    "fontsize": "30",
+    "labelfontsize":"30"
+}
 
-        edge_compute = IotGreengrass("Edge compute")
-        on_prem = ot_net >> Subnet("IT Network") >> edge_compute
+label_font_size="20"
+
+with Diagram("AWS SiteWise", show=False, graph_attr=graph_attr) as diag:
+    with Cluster("On-prem", graph_attr=cluster_graph_attr) as on_prem:
+        with Cluster("OT Network", graph_attr=cluster_graph_attr):
+            ot_net = Switch("HiOS 1", fontsize=label_font_size) \
+                >> Subnet("OT Network 1", fontsize=label_font_size) \
+                >> Firewall("Firewall 1", fontsize=label_font_size)
+
+        edge_compute = IotGreengrass("Edge compute", fontsize=label_font_size)
+        on_prem = ot_net \
+            >> Subnet("IT Network", fontsize=label_font_size) \
+            >> edge_compute
         
 
-    with Cluster("AWS Cloud"):
-        off_prem = on_prem >> IotSitewise("SiteWise") >> IotAnalytics("Dashboard")
-        edge_compute << IotDeviceManagement("Management")
+    with Cluster("AWS Cloud", graph_attr=cluster_graph_attr):
+        off_prem = on_prem \
+            >> IotSitewise("SiteWise", labelfloat="true", fontsize=label_font_size) \
+            >> IotAnalytics("Dashboard", fontsize=label_font_size)
+        edge_compute << IotDeviceManagement("Management", fontsize=label_font_size)
 
-    off_prem << Users("Net Admins")
+    off_prem << Users("Net Admins", fontsize=label_font_size)
